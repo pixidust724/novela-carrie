996aaad727eb33afedc9991b07987cba54f370f3
pixidust724 <pixidust724@gmail.com>
2020-10-08 17:29:24 +0200

    updated website

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,29 +5,25 @@ module.exports = {
     siteUrl: `https://carrielau.com/`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      heading: `Hello people, I'm Carrie. From 🇭🇰🇬🇧🇩🇪`,
       maxWidth: 652,
     },
     social: [
       {
-        name: `twitter`,
-        url: `https://twitter.com/narative`,
-      },
-      {
         name: `github`,
-        url: `https://github.com/narative`,
+        url: `https://github.com/pixidust724`,
       },
-      {
-        name: `instagram`,
-        url: `https://instagram.com/narative.co`,
-      },
+      // {
+      //   name: `instagram`,
+      //   url: `https://instagram.com/narative.co`,
+      // },
       {
         name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
+        url: `https://www.linkedin.com/in/claume/`,
       },
       {
         name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
+        url: `https://dribbble.com/claume`,
       },
     ],
   },
